28ebbb6e1ce56e31e38b2286dc2c3a4d4a2770d7
Cornelia Gherman <70709834+corneliag08@users.noreply.github.com>
2020-10-15 10:30:11 +0300

    Add files via upload

---
+++ problema02.py
@@ -0,0 +1,8 @@
+a=int(input("primul numar:"))
+b=int(input("al doilea numar:"))
+c=int(input("al treilea numar:"))
+if (a<b+c) and (b<a+c) and (c<b+a):
+    print("Da")
+else:
+     print("Nu")
+     

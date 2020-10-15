d15b4021f96ad92f01f606db4b7b98f9a5ed7bed
Cornelia Gherman <70709834+corneliag08@users.noreply.github.com>
2020-10-15 10:44:40 +0300

    Add files via upload

---
+++ problema04.py
@@ -0,0 +1,9 @@
+a=int(input("varsta  primei persoane :"))
+b=int(input("varsta la a doua persoana :"))
+c=int(input(" varsta la al treia persoana :"))
+if a>18 and a<60 :
+    print (a , 'ani')
+elif b>18 and b<60 :
+    print (b , 'ani')
+elif c>18 and c<60 :
+    print (c , 'ani')

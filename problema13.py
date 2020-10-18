f4017e8f3c0a09ea0c5a96a04358e6b14efe1dd3
Cornelia Gherman <70709834+corneliag08@users.noreply.github.com>
2020-10-18 16:27:52 +0300

    Add files via upload

---
+++ problema13.py
@@ -0,0 +1,11 @@
+y=int(input("Locul ocupat de Ionel :")) 
+if y>100: 
+    print("Ionel nu primeste tricou") 
+if (y%4==1) : 
+    print("Primeste tricou alb") 
+if (y%4==2) : 
+    print("Primeste tricou rosu") 
+if (y%4==3) : 
+    print("Primeste tricou albastru") 
+if (y%4==4) : 
+    print("Primeste tricou negru")

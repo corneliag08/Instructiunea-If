49fcfc46272ea8dce446e0a964d54013ee1bccff
Cornelia Gherman <70709834+corneliag08@users.noreply.github.com>
2020-10-18 13:13:35 +0300

    Add files via upload

---
+++ problema06.py
@@ -0,0 +1,10 @@
+a=int(input("Primul numar :"))
+ b=int(input("Al doilea numar :"))
+ c=int(input("Al treilea numar :"))
+if (a>0) and (b>0) and (c>0):
+    if (b>c):
+        print(b)
+        if (a<0) or (b<0) or (c<0) :
+        print (a+b)
+else : 
+    print (c)

7210d05145e56a9c52b1068f5ef066fb160a7115
Cornelia Gherman <70709834+corneliag08@users.noreply.github.com>
2020-10-18 16:21:45 +0300

    Add files via upload

---
+++ problema12.py
@@ -0,0 +1,11 @@
+p=int(input("Numarul de petale :")) 
+if (p%5==1): 
+    print("Ma iubeste un pic") 
+elif (p%5==2): 
+    print("Ma iubeste mult")
+elif (p%5==3): 
+    print("Ma iubeste cu pasiune") 
+elif (p%5==4): 
+    print("Ma iubeste la nebunie") 
+else:
+    print("Ma iubeste de loc") 
